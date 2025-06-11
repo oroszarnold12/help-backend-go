76b141c8f4173fd9b58058e957a999d25df9ce94
oroszarnold <arnold.orosz@onedot.com>
2025-06-11 15:47:44 +0300

    * added users seed

---
+++ cmd/migrate/migrations/20250611123637_seed-users.down.sql
@@ -0,0 +1,12 @@
+DELETE FROM users WHERE email IN (
+  'alice.student@help.com',
+  'bob.student@help.com',
+  'charlie.student@help.com',
+  'diana.student@help.com',
+  'ethan.student@help.com'
+  'fiona.teacher@help.com',
+  'george.teacher@help.com',
+  'hannah.teacher@help.com',
+  'isaac.teacher@help.com',
+  'julia.teacher@help.com'
+);
